93882a4b7ce2a1df9fb89a789f8ea8b310cd925b
김도연 <tyxmfh@naver.com>
2025-03-03 12:59:37 +0900

    Initial commit

---
+++ src/main/java/com/pod/playchild/PlaychildApplication.java
@@ -0,0 +1,14 @@
+package com.pod.playchild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//아놀자
+@SpringBootApplication
+public class PlaychildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaychildApplication.class, args);
+	}
+
+}
