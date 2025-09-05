726e1fa22febab958aa4b9131b7cab5f7d524e07
doyeonkim <tyxmfh@naver.com>
2025-09-05 15:05:56 +0900

    fix : new archi

--- src/main/java/com/pod/playchild/YujApplication.java
+++ src/main/java/com/pod/playchild/YujApplication.java
@@ -4,14 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-// 아놀자 개발
-// 2025.03.03 start.
-
 @SpringBootApplication()
-public class PlaychildApplication {
+public class YujApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PlaychildApplication.class, args);
+		SpringApplication.run(YujApplication.class, args);
 	}
 
 }
